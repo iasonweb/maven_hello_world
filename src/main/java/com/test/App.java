3d79107393653f059ae4801d46ce07786af28007
joeami <i.otkhozoria@yahoo.com>
2020-01-01 10:05:07 +0400

    some changes have been made

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -5,7 +5,14 @@ package com.test;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+    @Override
+    public void printLineToConsole() {
+        System.out.println("Hello World!");
     }
+    
+    public static void main( String[] args ) {
+        new App.printLineToConsole();
+    }
+    
 }
